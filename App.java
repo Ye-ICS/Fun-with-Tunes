8406e44d4dc93077fe34b103adc92afb113af6be
MrYe070 <102677547+MrYe070@users.noreply.github.com>
2025-10-28 12:32:07 -0400

    Create array for notes

--- App.java
+++ App.java
@@ -6,8 +6,16 @@ class App {
     }
 
     static void playTwinkle(int beatsPerMinute) {
+        int[] notes = {StdMidi.C4, StdMidi.C4, StdMidi.G4, StdMidi.G4, StdMidi.A4, StdMidi.A4, StdMidi.G4};
+
         StdMidi.setTempo(beatsPerMinute);
-        StdMidi.playNote(StdMidi.G4, 1);
+        StdMidi.playNote(notes[0], 1);
+        StdMidi.playNote(notes[1], 1);
+        StdMidi.playNote(notes[2], 1);
+        StdMidi.playNote(notes[3], 1);
+        StdMidi.playNote(notes[4], 1);
+        StdMidi.playNote(notes[5], 1);
+        StdMidi.playNote(notes[6], 1);
     }
 
     static void playMoonlightSonata (int beatsPerMinute) {
