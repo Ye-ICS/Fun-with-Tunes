6056f6c8ed00fb15633738c90b7b90ae165a0a1c
MrYe070 <102677547+MrYe070@users.noreply.github.com>
2025-11-17 11:36:53 -0500

    General callback method for preset tunes
    Declare bpm and volume sliders at class level

--- App.java
+++ App.java
@@ -5,13 +5,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    Slider bpmSlider;
+    Slider volumeSlider;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -36,9 +38,9 @@ public class App extends Application {
         VBox controlsBox = new VBox();
         controlsBox.setStyle("-fx-border-color: red; -fx-spacing: 20; -fx-padding: 10");
         Label bpmLabel = new Label("Tempo (BPM):");
-        Slider bpmSlider = new Slider(10, 480, 80);
+        bpmSlider = new Slider(10, 480, 80);
         Label volumeLabel = new Label("Volume:");
-        Slider volumeSlider = new Slider(0, 100, 70);
+        volumeSlider = new Slider(0, 100, 70);
         volumeSlider.setMajorTickUnit(50);
         volumeSlider.setShowTickLabels(true);
 
@@ -51,14 +53,8 @@ public class App extends Application {
         presetTunesBox.setCenter(tuneButtonsBox);
 
         // Reactions (aka callbacks):
-        twinkleBtn.setOnAction(event -> {
-            StdMidi.setVelocity((int) volumeSlider.getValue() * 127 / 100);
-            Tunes.playTwinkle((int)bpmSlider.getValue());
-        });
-        moonlightBtn.setOnAction(event -> {
-            StdMidi.setVelocity((int) volumeSlider.getValue() * 127 / 100);
-            Tunes.playMoonlightSonata((int)bpmSlider.getValue());
-        });
+        twinkleBtn.setOnAction(event -> playTune(0));
+        moonlightBtn.setOnAction(event -> playTune(1));
         userNotesBtn.setOnAction(event -> {
             StdMidi.setVelocity((int) volumeSlider.getValue() * 127 / 100);
             String note = noteBox.getText();
@@ -69,5 +65,20 @@ public class App extends Application {
         Scene scene = new Scene(presetTunesBox);
         stage.setScene(scene);
         stage.show();
+    }
+
+    /**
+     * Callback method to play a preset tune based on the given tune code.
+     * @param tuneCode 0 for Twinkle, 1 for Moonlight Sonata
+     */
+    void playTune(int tuneCode) {
+        int bpm = (int) bpmSlider.getValue();
+        int volume = (int) volumeSlider.getValue();
+        StdMidi.setVelocity(volume * 127 / 100);
+        switch (tuneCode) {
+            case 0 -> Tunes.playTwinkle(bpm);
+            case 1 -> Tunes.playMoonlightSonata(bpm);
+            default -> System.out.println("Invalid tune code");
+        }
     }
 }
