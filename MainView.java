57f0282a603f46f46082a84d0cf9a2a62e334eac
MrYe070 <102677547+MrYe070@users.noreply.github.com>
2025-12-12 12:18:18 -0500

    Create MainView custom node
    Moving all GUI into MainView

---
+++ MainView.java
@@ -0,0 +1,274 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+class MainView extends BorderPane {
+    static final String[] keyTexts = "QWERTYUASDFGHJZXCVBNM".split("");
+    static final String[] keyNotes = "C3,D3,E3,F3,G3,A3,B3,C4,D4,E4,F4,G4,A4,B4,C5,D5,E5,F5,G5,A5,B5".split(",");
+    boolean[] noteIsPlaying = new boolean[keyNotes.length];
+    Thread backgroundTuneThread = null;
+    Slider bpmSlider;
+    Slider volumeSlider;
+    TextField notesBox;
+    TextField durationsBox;
+    Text fileText;
+    File tuneFile;
+
+    
+    MainView() {
+       
+        setStyle("-fx-border-color: black; -fx-border-width: 2");
+        
+        Label tunesLabel = new Label("Preset Tunes");
+        tunesLabel.setStyle("-fx-font-size: 20; -fx-font-weight: bold; -fx-border-color: blue");
+        
+        FlowPane tuneButtonsBox = new FlowPane();
+        tuneButtonsBox.setStyle("-fx-border-color: green; -fx-padding: 10; -fx-hgap: 20; -fx-vgap: 20");
+        Button twinkleBtn = new Button("Twinkle");
+        Button moonlightBtn = new Button("Moonlight Sonata");
+        Button stopBtn = new Button("Stop Tune");
+
+        VBox userNotesBox = new VBox();
+        Label noteLabel = new Label("Custom notes:");
+        notesBox = new TextField();
+        Label durationsLabel = new Label("Durations:");
+        durationsBox = new TextField();
+        Button userNotesBtn = new Button("Play Notes");
+        Button saveToFileBtn = new Button("Save to File");
+
+        fileText = new Text();
+        Button fileSelectBtn = new Button("Open File");
+        Button filePlayBtn = new Button("Play File");
+
+        Node keyboard = createKeysRow();   
+        keyboard.setStyle("-fx-border-color: cyan");     
+        
+        VBox controlsBox = new VBox();
+        controlsBox.setStyle("-fx-border-color: red; -fx-spacing: 20; -fx-padding: 10");
+        Label bpmLabel = new Label("Tempo (BPM):");
+        bpmSlider = new Slider(10, 480, 80);
+        Label volumeLabel = new Label("Volume:");
+        volumeSlider = new Slider(0, 100, 70);
+        volumeSlider.setMajorTickUnit(50);
+        volumeSlider.setShowTickLabels(true);
+
+        // Organize components in containers.
+        setTop(tunesLabel);
+        BorderPane.setAlignment(tunesLabel, Pos.CENTER);
+        controlsBox.getChildren().addAll(bpmLabel, bpmSlider, volumeLabel, volumeSlider);
+        setRight(controlsBox);
+        userNotesBox.getChildren().addAll(noteLabel, notesBox, durationsLabel, durationsBox, userNotesBtn, saveToFileBtn);
+        tuneButtonsBox.getChildren().addAll(twinkleBtn, moonlightBtn, stopBtn, userNotesBox, fileText, fileSelectBtn, filePlayBtn, keyboard);
+        setCenter(tuneButtonsBox);
+
+        // Reactions (aka callbacks):
+        twinkleBtn.setOnAction(event -> playTune(0));
+        moonlightBtn.setOnAction(event -> playTune(1));
+        stopBtn.setOnAction(event -> stopTune());
+        userNotesBtn.setOnAction(event -> playUserNotes());
+        saveToFileBtn.setOnAction(event -> saveTuneToFile());
+        fileSelectBtn.setOnAction(event -> openFileChooser());
+        filePlayBtn.setOnAction(event -> playTuneFile());
+        keyboard.setOnKeyPressed(keyEvent -> noteDownFromKey(keyEvent.getText()));
+        keyboard.setOnKeyReleased(keyEvent -> noteUpFromKey(keyEvent.getText()));
+    }
+
+    /**
+     * Callback method to play user-entered notes.
+     */
+    void playUserNotes() {
+        StdMidi.setTempo((int) bpmSlider.getValue());
+        StdMidi.setVelocity((int) volumeSlider.getValue() * 127 / 100);
+        String notesLine = notesBox.getText();
+        String durationsLine = durationsBox.getText();
+
+        try {
+            String[] notes = notesLine.split(",");
+            double[] durations = Utils.parseDoubles(durationsLine.split(","));
+            Tunes.playNotes(notes, durations);
+        } catch (NumberFormatException nfe) {
+            System.err.println("Invalid durations.");   // TODO: Show a popup alert.
+            return;
+        } catch (IllegalArgumentException iae) {
+            System.err.println(iae.getMessage());   // TODO: Show a popup alert.
+            return;
+        }
+    }
+
+    /**
+     * Callback method to play a preset tune based on the given tune code.
+     * @param tuneCode 0 for Twinkle, 1 for Moonlight Sonata
+     */
+    void playTune(int tuneCode) {
+        if (backgroundTuneThread != null && backgroundTuneThread.isAlive()) {
+            System.out.println("A tune is already playing...");
+            return;
+        }
+
+        int bpm = (int) bpmSlider.getValue();
+        int volume = (int) volumeSlider.getValue();
+        StdMidi.setVelocity(volume * 127 / 100);
+        backgroundTuneThread = new Thread(() -> {
+            switch (tuneCode) {
+                case 0 -> Tunes.playTwinkle(bpm);
+                case 1 -> Tunes.playMoonlightSonata(bpm);
+                default -> System.out.println("Invalid tune code");
+            }
+        });
+        backgroundTuneThread.start();
+    }
+
+    void stopTune() {
+        if (backgroundTuneThread == null) {
+            return; // Nothing to stop
+        }
+
+        backgroundTuneThread.stop();
+        backgroundTuneThread = null;
+        StdMidi.allNotesOff();  // Turn off any lingering notes.
+
+    }
+
+    /**
+     * Callback method to play previously selected tune file.
+     */
+    void playTuneFile() {
+        if (tuneFile == null) {
+            System.out.println("No file selected. Openning file chooser instead");
+            openFileChooser();
+            return;
+        }
+
+        Tunes.playFile(tuneFile);
+    }
+
+    /**
+     * Callback to open file chooser and set `tuneFile` to selected file.
+     */
+    void openFileChooser() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Tune File");
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            tuneFile = selectedFile;
+            fileText.setText(tuneFile.getName());
+        }
+    }
+
+    /**
+     * Callback to open file saver dialog and save current user tune to file.
+     */
+    void saveTuneToFile() {
+        // Get the notes and durations verify they are valid.
+        String notesLine = notesBox.getText();
+        String durationsLine = durationsBox.getText();
+        String[] notes = notesLine.split(",");
+        double[] durations;
+        
+        // TODO: This section deserves its own method in Tunes.java!
+        try {
+            Tunes.getNoteCodes(notes); // Not using the codes, just verifying notes are valid
+            durations = Utils.parseDoubles(durationsLine.split(","));
+        } catch (NumberFormatException nfe) {
+            System.err.println("Invalid durations.");   // TODO: Show a popup alert.
+            return;
+        } catch (IllegalArgumentException iae) {
+            System.err.println(iae.getMessage());   // TODO: Show a popup alert.
+            return;
+        }
+
+        if (durations.length != notes.length) {
+            System.err.println("Number of durations must match number of notes."); // TODO: Show a popup alert.
+            return;
+        }
+        
+        // Open file chooser to let user choose location and file name.
+        FileChooser newfileChooser = new FileChooser();
+        File saveFile = newfileChooser.showSaveDialog(null);
+        if (saveFile == null) { // User cancelled
+            return; 
+        }
+
+        // Save to file.
+        int bpm = (int) bpmSlider.getValue();
+        try {
+            Tunes.saveToFile(saveFile, notes, durations, bpm);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Invalid path??? How? " + saveFile.getAbsolutePath());
+        }
+    }
+
+    /**
+     * Creates a keyboard layout.
+     * @return Node keyboard layout.
+     */
+    Node createKeysRow() {
+        HBox keysRow = new HBox();  // You may use a different layout if you wish.
+
+        for (int i = 0; i < keyNotes.length; i++) {
+            Button key = new Button(keyTexts[i] + "\n" + keyNotes[i]);
+            key.setStyle("-fx-pref-height: 120; -fx-font-size: 10; -fx-font-weight: bold; -fx-text-fill: black; -fx-text-alignment: center; -fx-background-color: ivory; -fx-padding: 5;");
+            keysRow.getChildren().add(key);
+            int noteCode = Notes.getNoteCode(keyNotes[i]);
+            key.setOnMousePressed(event -> noteDown(noteCode));
+            key.setOnMouseReleased(event -> noteUp(noteCode));
+        }
+        return keysRow;
+    }
+
+    void noteDown(int noteCode) {
+        System.out.println("Note on: " + noteCode);
+        StdMidi.setVelocity((int) volumeSlider.getValue() * 127 / 100);
+        StdMidi.noteOn(noteCode);
+    }
+
+    void noteUp(int noteCode) {
+        System.out.println("Note off: " + noteCode);
+        StdMidi.noteOff(noteCode);
+    }
+
+    /**
+     * Start note corresponding to pressed key, if not already started.
+     * @param keyText Name of key pressed. Eg: Q, W, E, etc.
+     */
+    void noteDownFromKey(String keyText) {
+        // Find key in keyTexts array matching keyText
+        for (int i = 0; i < keyTexts.length; i++) {
+            if (keyTexts[i].equalsIgnoreCase(keyText) && !noteIsPlaying[i]) {  // if found and not already playing
+                int noteCode = Notes.getNoteCode(keyNotes[i]);
+                noteDown(noteCode);
+                noteIsPlaying[i] = true;   // Mark note as playing
+                return;
+            }
+        }
+    }
+
+    /**
+     * Stop note corresponding to released key.
+     * @param keyText Name of key released. Eg: Q, W, E, etc.
+     */
+    void noteUpFromKey(String keyText) {
+        // Find key in keyTexts array matching keyText
+        for (int i = 0; i < keyTexts.length; i++) {
+            if (keyTexts[i].equalsIgnoreCase(keyText) && noteIsPlaying[i]) {  // if found and currently playing
+                int noteCode = Notes.getNoteCode(keyNotes[i]);
+                noteUp(noteCode);
+                noteIsPlaying[i] = false;   // Mark note as not playing
+                return;
+            }
+        }
+    }
+}
